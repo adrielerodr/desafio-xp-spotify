bf0c3875f13e02380cd685cd77d0210ea59d4325
AdrieleSRodrigues <adriele.rodrigues@accenture.com>
2019-04-13 19:15:20 -0300

    Add style Serach page

--- src/features/search/views/Search.view.js
+++ src/features/search/views/Search.view.js
@@ -4,18 +4,23 @@ import ResultView from './Result.view';
 
 const SearchView = ({ fields, lists, handlers, album }) => {
   return (
-    <div>
-      <form>
+    <div className="view">
+      <form className="search-input">
         <label>Busque por artistas, álbuns ou músicas</label>
         <input
           type="text"
           name="search"
-          placeholder="Comece a escrever..."
           value={fields.search}
+          placeholder="Comece a escrever..."
           onChange={handlers.setField('search')}
         />
       </form>
-      <div>
+      {lists.results.length > 0 && (
+        <div className="header-search">
+          <h3>Resultados encontrados para "{fields.search}"</h3>
+        </div>
+      )}
+      <div className="results">
         {lists.results.length > 0
           ? lists.results.map(r => (
               <ResultView
